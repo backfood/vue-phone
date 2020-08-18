b0c83fb0538ede79ebdc28af519c60fb7f1bcfbf
web <835959138@qq.com>
2020-08-18 19:52:22 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <div ref="msgDiv">{{msg}}</div>
+    <div v-if="msg1">Message got outside $nextTick: {{msg1}}</div>
+    <div v-if="msg2">Message got inside $nextTick: {{msg2}}</div>
+    <div v-if="msg3">Message got outside $nextTick: {{msg3}}</div>
+    <button @click="changeMsg">Change the Message</button> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      msg: "Hello Vue.",
+      msg1: "",
+      msg2: "",
+      msg3: "",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    changeMsg() {
+      this.msg = "Hello world.";
+      // console.log(this.$refs.msgDiv.innerHTML)
+      this.msg1 = this.$refs.msgDiv.innerHTML;
+      this.$nextTick(() => {
+        this.msg2 = this.$refs.msgDiv.innerHTML;
+      });
+      this.msg3 = this.$refs.msgDiv.innerHTML;
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,6 +47,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 75px;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+a {
+  font-size: 28px;
+  margin: 0;
+  padding: 0;
 }
 </style>
